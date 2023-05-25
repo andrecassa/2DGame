8469a099e41a00b3a8455713d80ad62e8cad5fa4
andrecassa <acasarini2002@gmail.com>
2023-05-25 15:35:01 +0200

    Update

---
+++ src/main/java/com/casarini/game/states/MenuState.java
@@ -0,0 +1,27 @@
+package com.casarini.game.states;
+
+import com.casarini.game.util.KeyHandler;
+import com.casarini.game.util.MouseHandler;
+
+import java.awt.*;
+
+public class MenuState extends GameState {
+    public MenuState(GameStateManager gameStateManager) {
+        super(gameStateManager);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void input(MouseHandler mouse, KeyHandler key) {
+
+    }
+
+    @Override
+    public void render(Graphics2D g) {
+
+    }
+}
